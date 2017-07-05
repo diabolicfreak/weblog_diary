964b9652fd9f2c28ddfd7c6af35a6624ef285c2a
diabolicfreak <diabolicfreak@gmail.com>
2017-07-05 14:56:12 +0530

    Server up and running

---
+++ config/database.js
@@ -0,0 +1,13 @@
+var mongoose = require('mongoose');
+
+var dbUri = 'mongodb://localhost:27017/lordvcs-blog';
+
+mongoose.connection.openUri(dbUri, function(err, res){
+    if (err) {
+        console.log ('ERROR connecting to: ' + dbUri + '. ' + err);
+    } else {
+        console.log ('Succeeded connected to: ' + dbUri);
+    }
+});
+
+module.exports = mongoose;
